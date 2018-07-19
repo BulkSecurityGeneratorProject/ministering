9ddc9f40d2f95a8c7d6e2cf5f786de465b796347
David Q. Romney <dromney@incomm.com>
2018-07-19 13:26:14 -0600

    Updated db_scripts for family record, population script.

--- docs/db_scripts.sql
+++ docs/db_scripts.sql
@@ -259,3 +259,64 @@ alter table ministry
 ;
 
 
+-- ------------------------------------------------------------------------------
+-- Population script
+-- ------------------------------------------------------------------------------
+INSERT INTO ministering.org (id, name) VALUES (1, 'Copperhill 10th Ward');
+
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (1, 'Romney', 'David and Christine', '4033 South Powderwood Drive, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (2, 'King', 'Peter and Lori', '4226 Williamsburg Drive, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (3, 'Beynon', 'Annette', '4240 South 6400 West, West Valley City, Utah 84128-6524');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (4, 'Kelly', 'Norman and Jill', '4213 South 6820 West, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (5, 'Sandstrom', 'Chris and Angela', '6564 West Brookview Circle, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (6, 'Smith', 'Tim and Mary', '4277 South 6220 West, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (7, 'Rackley', 'Donna', '4240 South 6400 West, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (8, 'Beckstrom', 'Scott and Dianna', '6498 West 4200 South, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (9, 'Brown', 'Scott and Lisa', '6314 West King Valley Drive, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (10, 'Fischer', 'Melissa Anne', '6486 West 4200 South, West Valley City, Utah 84128');
+INSERT INTO ministering.family (id, name, couple_name, address) VALUES (11, 'Schwab', 'Richard and Jessica', '6334 West 4215 South, West Valley City, Utah 84128');
+
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (1, 1, 1, 'HEAD_OF_HOUSE', 'David', 'Quenten', 'Romney', '1962-08-29');
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (2, 1, 1, 'SPOUSE', 'Christine', 'H', 'Romney', '1961-12-29');
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (3, 1, 2, 'HEAD_OF_HOUSE', 'Peter', null, 'King', null);
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (4, 1, 2, 'SPOUSE', 'Lori', null, 'King', null);
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (5, 1, 8, 'HEAD_OF_HOUSE', 'Scott', null, 'Beckstrom', '1962-01-01');
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (6, 1, 8, 'SPOUSE', 'Dianna', null, 'Beckstrom', '1972-01-01');
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (7, 1, 8, 'CHILD', 'Kyle', null, 'Beckstrom', '1998-01-01');
+INSERT INTO ministering.member (id, org_id, family_id, type, first_name, middle_name, last_name, birthdate) VALUES (8, 1, 4, 'CHILD', 'Adam', null, 'King', null);
+
+INSERT INTO ministering.email (id, member_id, type, address) VALUES (1, 1, 'INDIVIDUAL', 'dqromney@gmail.com');
+INSERT INTO ministering.email (id, member_id, type, address) VALUES (2, 2, 'INDIVIDUAL', 'hatchc4@gmail.com');
+
+INSERT INTO ministering.phone (id, member_id, type, number, text_msg_okay) VALUES (1, 1, 'MOBILE', '801-834-4855', 1);
+INSERT INTO ministering.phone (id, member_id, type, number, text_msg_okay) VALUES (2, 2, 'MOBILE', '801-834-1084', 1);
+INSERT INTO ministering.phone (id, member_id, type, number, text_msg_okay) VALUES (3, 1, 'LAND_LINE', '801-967-1692', 0);
+INSERT INTO ministering.phone (id, member_id, type, number, text_msg_okay) VALUES (4, 2, 'LAND_LINE', '801-967-1692', 0);
+
+INSERT INTO ministering.companionship (id, name) VALUES (1, 'Romney/King');
+INSERT INTO ministering.companionship (id, name) VALUES (2, 'Beckstrom/Beckstrom/King');
+
+INSERT INTO ministering.companion (id, member_id, companionship_id) VALUES (1, 1, 1);
+INSERT INTO ministering.companion (id, member_id, companionship_id) VALUES (2, 3, 1);
+INSERT INTO ministering.companion (id, member_id, companionship_id) VALUES (3, 5, 2);
+INSERT INTO ministering.companion (id, member_id, companionship_id) VALUES (4, 7, 2);
+INSERT INTO ministering.companion (id, member_id, companionship_id) VALUES (5, 8, 2);
+
+INSERT INTO ministering.stewardship (id, companionship_id) VALUES (1, 1);
+INSERT INTO ministering.stewardship (id, companionship_id) VALUES (2, 2);
+
+INSERT INTO ministering.ministry (id, member_id, companionship_id, stewardship_id) VALUES (1, 1, 1, 1);
+INSERT INTO ministering.ministry (id, member_id, companionship_id, stewardship_id) VALUES (2, 3, 1, 1);
+INSERT INTO ministering.ministry (id, member_id, companionship_id, stewardship_id) VALUES (3, 5, 2, 2);
+INSERT INTO ministering.ministry (id, member_id, companionship_id, stewardship_id) VALUES (4, 7, 2, 2);
+INSERT INTO ministering.ministry (id, member_id, companionship_id, stewardship_id) VALUES (5, 8, 2, 2);
+
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (1, 1, 3);
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (2, 1, 4);
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (3, 1, 5);
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (4, 1, 6);
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (5, 1, 7);
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (6, 2, 9);
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (7, 2, 10);
+INSERT INTO ministering.assignment (id, stewardship_id, family_id) VALUES (8, 2, 11);
+
